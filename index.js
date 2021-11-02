31d48e1d2ab29c204e2679967425a6f683432ee5
Pedro Delgado <pedro.delgado2017@mesacharter.org>
2021-11-02 18:15:48 +0000

    completed yesterdays code challenge today

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
 //1
-function greatestOfTwo(){
-  
+function greatestOfTwo(x,y){
+  if( x > y){
+      return x
+  } else{
+      return y
+  }
 }
 
 //2
-function greatestOfThree(){
-  
+function greatestOfThree(a,b,c){
+  if(( a > b) && (a > c)){
+      return a
+  } else if((b > a) && ( b > c)){
+      return b
+  } else{
+      return c
+  }
 }
-
 //3
-function greatestOfFour(){
-  
+function greatestOfFour(a,b,c,d){
+  if(( a > b) && (a > c) && (a > d)){
+      return a
+  } else if((b > a) && ( b > c) && (b > d)){
+      return b
+  } else if((c > a) && (c > b) && (c > d)){
+      return c
+  } else{
+      return d
+  }
 }
 
 
 //4
-function leastOfFour(){
-  
+function leastOfFour(a,b,c,d){
+  if(( a < b) && (a < c) && (a < d)){
+      return a
+  } else if((b < a) && ( b < c) && (b < d)){
+      return b
+  } else if((c < a) && (c < b) && (c < d)){
+      return c
+  } else{
+      return d
+  }
 }
+
+console.log(greatestOfTwo(9,8))
+console.log(greatestOfThree(5,2,3))
+console.log(greatestOfFour(7,2,3,1))
+console.log(leastOfFour(38,34,46,80))
